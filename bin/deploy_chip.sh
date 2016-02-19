1b36e88bcc75b8d449b1942dddc46f625567fe88
Deepak Guruswamy <deepak.kamesh@gmail.com>
2016-02-19 00:53:51 -0800

    removing local deploy script

--- bin/deploy_chip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "shipping viki to chip..."
-
-ssh chip@chip sudo pkill vikid
-scp vikid chip@chip:~/viki/
-scp objects.conf chip@chip:~/viki/
-scp ../resources/* chip@chip:~/viki/resources/
-
-
