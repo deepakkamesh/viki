55f768b2e6ee5a14dc951641b427d97d7182e1b9
Deepak Guruswamy <deepak.kamesh@gmail.com>
2017-05-12 14:19:43 -0700

    fix minor issue on remote

--- bin/run_viki.sh
+++ bin/run_viki.sh
@@ -5,9 +5,14 @@ LOC=$(dirname "$0")
 $LOC/vikid \
 -config_file=$LOC/objects.conf \
 -log=$LOC/../logs \
+-log_file=$LOC/../logs/viki.log \
+-log_stdout=false \
 -graphite_ipport=metrics.hyperlinkhome.com:2003 \
 -resource=$LOC/../resources \
--log_stdout=true \
--log_file=$LOC/../logs/viki.log
-#&
+-v=2 \
+-stderrthreshold=info \
+-alsologtostderr=false \
+-logtostderr=false \
+-log_dir=$LOC/../logs \
+&
 #-ssl \
--- my_remote_control.go
+++ my_remote_control.go
@@ -14,7 +14,7 @@ func (m *Viki) MyRemoteCode(c chan devicemanager.DeviceData) {
 		select {
 		// Channel to recieve any events.
 		case got := <-c:
-			name, _ := m.ObjectManager.GetObjectByName(got.Object)
+			name, _ := m.ObjectManager.GetObjectByAddress(got.Object)
 
 			switch name {
 
